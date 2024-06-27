7def3de81080bea083caea19626992accb3e53f5
Devine <devineparadoxz@gmail.com>
2024-06-27 09:46:12 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+API_ID = 29230755
+API_HASH = "ab41c7247a91680d2d0dc705ad7158da"
+BOT_TOKEN = "7315074622:AAGog60wADjA2rxkSgqqD9yJ3oXMikw3YVI"
+OPENAI_KEY = "sk-proj-1e9GtG7JQ8tOOdwBAGroT3BlbkFJAc9dNDl1PWVctAajuy6G"
+BOT_USERNAME = "DevineChatGpt_Bot"
+OWNER_ID = 6440363814
+BOT_NAME = "devine"
+START_IMG = "https://telegra.ph/file/5632b2e02f0a72431ed97.jpg"
+BANNED_USERS = None
+UPDATE_CHNL = "https://t.me/Devine_Network"
+OWNER_USERNAME = "fake_devine"
+SUPPORT_GROUP = "http://t.me/Devine_Community"
