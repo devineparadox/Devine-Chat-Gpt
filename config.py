aaca3451e8ac72611e5e5e423f7106bd303e4127
Devine <devineparadoxz@gmail.com>
2024-06-27 12:01:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 API_ID = 29230755
 API_HASH = "ab41c7247a91680d2d0dc705ad7158da"
 BOT_TOKEN = "6789332192:AAEentii9Dfk13yjGXHbWAJsIyZRPaqu8HA"
-OPENAI_KEY = "sk-proj-1e9GtG7JQ8tOOdwBAGroT3BlbkFJAc9dNDl1PWVctAajuy6G"
+OPENAI_KEY = "sk-telegram-YdHWxNRVYnemKUt6MH0kT3BlbkFJ7J1l2FXsSH9okH2lkj5D"
 BOT_USERNAME = "kwsgj176bot"
 OWNER_ID = 6440363814
 BOT_NAME = "okk ok"
-START_IMG = "https://telegra.ph/file/5632b2e02f0a72431ed97.jpg"
+START_IMG = "hhttps://graph.org/file/3b4ddf531eeb2031ce727.jpg"
 BANNED_USERS = None
-UPDATE_CHANNEL = "https://t.me/Devine_Network"
+UPDATE_CHANNEL = "Devine_Network"
 OWNER_USERNAME = "fake_devine"
-SUPPORT_GROUP = "http://t.me/Devine_Community"
+SUPPORT_GROUP = "Devine_Community"
 
  
