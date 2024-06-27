235e0a8cecb3c3ec430d51630cc05b2a321ae894
Devine <devineparadoxz@gmail.com>
2024-06-27 11:57:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 API_ID = 29230755
 API_HASH = "ab41c7247a91680d2d0dc705ad7158da"
-BOT_TOKEN = "7315074622:AAGog60wADjA2rxkSgqqD9yJ3oXMikw3YVI"
+BOT_TOKEN = "6789332192:AAEentii9Dfk13yjGXHbWAJsIyZRPaqu8HA"
 OPENAI_KEY = "sk-proj-1e9GtG7JQ8tOOdwBAGroT3BlbkFJAc9dNDl1PWVctAajuy6G"
-BOT_USERNAME = "DevineChatGpt_Bot"
+BOT_USERNAME = "kwsgj176bot"
 OWNER_ID = 6440363814
-BOT_NAME = "devine"
+BOT_NAME = "okk ok"
 START_IMG = "https://telegra.ph/file/5632b2e02f0a72431ed97.jpg"
 BANNED_USERS = None
 UPDATE_CHANNEL = "https://t.me/Devine_Network"
