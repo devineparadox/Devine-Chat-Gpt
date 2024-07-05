b0b75f27c53c023ccfaffd0702645691fe31c4f9
Devine <devineparadoxz@gmail.com>
2024-07-05 19:56:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,20 @@
-API_ID = 29230755
-API_HASH = "ab41c7247a91680d2d0dc705ad7158da"
-BOT_TOKEN = "7299207327:AAFqREhMPtnP0IPVfzupfx25MK6NGh0FuZM"
-OPENAI_KEY = "sk-devinedarl-anF8ZxuJGFWdrSuVcQoHT3BlbkFJSsYuT5GqvaxSnZR5Z499"
-BOT_USERNAME = "ryuprox_bot"
-OWNER_ID = 6440363814
-BOT_NAME = "Ꭰᴇᴠɪɴᴇ Ꮯʜᴀᴛ Ꮐᴘᴛ"
+# Get this value from my.telegram.org/appsAPI_ID = 
+API_ID = 
+# value = 926917
+API_HASH = ""
+# value = "ab41c9275a91680d2d0dc105ad0388da"
+
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = ""
+BOT_NAME = ""
+BOT_USERNAME = ""
+
+# Get this value from https://openai.com
+OPENAI_KEY = ""
+
+# Get this value on Telegram by /id
+OWNER_ID = 872628292
+
 START_IMG = "https://graph.org/file/3b4ddf531eeb2031ce727.jpg"
 BANNED_USERS = None
 UPDATE_CHANNEL = "Devine_Network"
