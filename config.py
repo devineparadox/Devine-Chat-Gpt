66899a5c6d7d1ff405c218a3b55b2f87610d4654
Devine <devineparadoxz@gmail.com>
2024-07-05 21:12:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,29 +15,5 @@ OPENAI_KEY = os.getenv('OPENAI_KEY')
 # Get this value on Telegram 
 OWNER_ID = int(os.getenv('OWNER_ID'))
 OWNER_USERNAME = os.getenv('OWNER_USERNAME')
-UPDATE_CHANNEL = os.getenv('UPDATE_CHNL')
+UPDATE_CHANNEL = os.getenv('UPDATE_CHANNEL')
 SUPPORT_GROUP = os.getenv('SUPPORT_GROUP')
-
-API_ID = 21264701
-# value = 926917
-API_HASH = ""
-# value = "ab41c9275a91680d2d0dc105ad0388da"
-
-# Get your token from @BotFather on Telegram.
-BOT_TOKEN = ""
-""
-BOT_USERNAME = ""
-
-# Get this value from https://openai.com
-OPENAI_KEY = ""
-
-# Get this value on Telegram by /id
-OWNER_ID = 872628292
-
-START_IMG = "https://graph.org/file/3b4ddf531eeb2031ce727.jpg"
-BANNED_USERS = None
- = "Devine_Network"
-OWNER_USERNAME = "devine_dark"
- = "Devine_Community"
-
- 
