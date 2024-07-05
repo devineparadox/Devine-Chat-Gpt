e62022b9d99e964cdc72890d833b8eed16b910b6
Devine <devineparadoxz@gmail.com>
2024-07-05 21:11:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,23 @@
+import os
+
 # Get this value from my.telegram.org/appsAPI_ID = 
+API_ID = os.getenv('API_ID')
+API_HASH = os.getenv('API_HASH')
+
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+BOT_NAME = os.getenv('BOT_NAME')
+BOT_USERNAME = os.getenv('BOT_USERNAME')
+
+# Get this value from https://openai.com
+OPENAI_KEY = os.getenv('OPENAI_KEY')
+
+# Get this value on Telegram 
+OWNER_ID = int(os.getenv('OWNER_ID'))
+OWNER_USERNAME = os.getenv('OWNER_USERNAME')
+UPDATE_CHANNEL = os.getenv('UPDATE_CHNL')
+SUPPORT_GROUP = os.getenv('SUPPORT_GROUP')
+
 API_ID = 21264701
 # value = 926917
 API_HASH = ""
@@ -6,7 +25,7 @@ API_HASH = ""
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = ""
-BOT_NAME = ""
+""
 BOT_USERNAME = ""
 
 # Get this value from https://openai.com
@@ -17,8 +36,8 @@ OWNER_ID = 872628292
 
 START_IMG = "https://graph.org/file/3b4ddf531eeb2031ce727.jpg"
 BANNED_USERS = None
-UPDATE_CHANNEL = "Devine_Network"
+ = "Devine_Network"
 OWNER_USERNAME = "devine_dark"
-SUPPORT_GROUP = "Devine_Community"
+ = "Devine_Community"
 
  
